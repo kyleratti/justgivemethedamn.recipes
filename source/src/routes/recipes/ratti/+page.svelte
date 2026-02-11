77eb6c384124ec912a6dfd7ee411f99da7444ce5
Kyle Ratti <kyleratti@users.noreply.github.com>
2026-02-10 19:27:22 -0500

    feat: add taco seasoning recipe

--- source/src/routes/recipes/ratti/+page.svelte
+++ source/src/routes/recipes/ratti/+page.svelte
@@ -11,32 +11,37 @@
 <p>These recipes are from the Ratti family.</p>
 
 <ul class="list-inside list-disc">
-	<li class="md:my-1 my-2">
+	<li class="my-2 md:my-1">
 		<a href="/recipes/ratti/chris-caramel-bars" class="text-blue-500 hover:underline"
 			>Chris' Caramel Bars</a
 		>
 	</li>
-	<li class="md:my-1 my-2">
+	<li class="my-2 md:my-1">
 		<a href="/recipes/ratti/chris-salsa" class="text-blue-500 hover:underline">Chris' Salsa</a>
 	</li>
-	<li class="md:my-1 my-2">
+	<li class="my-2 md:my-1">
 		<a href="/recipes/ratti/grandma-wests-baked-beans" class="text-blue-500 hover:underline"
 			>Grandma West's Baked Beans</a
 		>
 	</li>
-	<li class="md:my-1 my-2">
+	<li class="my-2 md:my-1">
 		<a href="/recipes/ratti/laurens-chicken-pot-pie" class="text-blue-500 hover:underline"
 			>Lauren's Chicken Pot Pie</a
 		>
 	</li>
-	<li class="md:my-1 my-2">
+	<li class="my-2 md:my-1">
 		<a href="/recipes/ratti/kyles-chicken-brine" class="text-blue-500 hover:underline">
 			Kyle's Chicken Brine
 		</a>
 	</li>
-	<li class="md:my-1 my-2">
+	<li class="my-2 md:my-1">
 		<a href="/recipes/ratti/kyles-ginger-snaps" class="text-blue-500 hover:underline">
 			Kyle's Ginger Snaps
+		</a>
+	</li>
+	<li class="my-2 md:my-1">
+		<a href="/recipes/ratti/kyles-taco-seasoning" class="text-blue-500 hover:underline">
+			Kyle's Taco Seasoning
 		</a>
 	</li>
 </ul>
---
+++ source/src/routes/recipes/ratti/kyles-taco-seasoning/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import IngredientsList from "$components/IngredientsList.svelte";
+	import RecipeTime from "$components/RecipeTime.svelte";
+	import StepsList from "$components/StepsList.svelte";
+	import { createTitle } from "$lib/titleUtil";
+	import type { Contributor, CookingRecipe } from "$lib/types";
+	import Fraction from "fraction.js";
+	import RecipeNote from "$components/RecipeNote.svelte";
+
+	const contributors: Contributor[] = [{ kind: "person", name: "Kyle" }];
+
+	const recipe: CookingRecipe = {
+		prepTimeMinutes: 5,
+		cookTimeMinutes: 0,
+		ingredients: [
+			{ kind: "exact", quantity: 1, unit: "tablespoon", name: "chili powder" },
+			{ kind: "exact", quantity: new Fraction(3, 2), unit: "teaspoon", name: "ground cumin" },
+			{ kind: "exact", quantity: 1, unit: "teaspoon", name: "sea salt" },
+			{ kind: "exact", quantity: 1, unit: "teaspoon", name: "ground black pepper" },
+			{ kind: "exact", quantity: new Fraction(1, 2), unit: "teaspoon", name: "ground paprika" },
+			{ kind: "exact", quantity: new Fraction(1, 4), unit: "teaspoon", name: "garlic powder" },
+			{ kind: "exact", quantity: new Fraction(1, 4), unit: "teaspoon", name: "onion powder" },
+			{
+				kind: "exact",
+				quantity: new Fraction(1, 4),
+				unit: "teaspoon",
+				name: "crushed red pepper flakes",
+			},
+			{ kind: "exact", quantity: new Fraction(1, 4), unit: "teaspoon", name: "dried oregano" },
+		],
+		steps: [{ description: "Mix all ingredients together." }],
+	};
+</script>
+
+<svelte:head>
+	<title>{createTitle("Kyle's Taco Seasoning")}</title>
+</svelte:head>
+
+<h2 class="mb-2 text-2xl font-bold">Kyle's Taco Seasoning</h2>
+
+<div class="mb-4 rounded-md border border-slate-200 bg-slate-50">
+	<RecipeTime prepTimeMinutes={recipe.prepTimeMinutes} cookTimeMinutes={recipe.cookTimeMinutes} />
+</div>
+
+<div class="mb-4 rounded-md border border-slate-200 bg-slate-50 p-4 md:p-8">
+	<IngredientsList ingredients={recipe.ingredients} />
+</div>
+
+<div class="mb-4 rounded-md border border-slate-200 bg-slate-50 p-4 md:p-8">
+	<StepsList steps={recipe.steps} />
+</div>
